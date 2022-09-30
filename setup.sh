22cbd172bca28a66ee8eebe06fcee582df1bc6a3
JunHatanaka <nextseason6@gmail.com>
2022-10-01 04:17:57 +0900

    Add: コマンド1発で環境構築OK

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# コマンドラインで実行必要なもののみ。ファイル作成が必要なものはファイルを置く。
+git init
+docker-compose build
+docker-compose run web rails new . --force --no-deps --database=mysql
+cp database_yml_template.txt config/database.yml
+docker-compose run web rails webpacker:install
+docker-compose run web rails db:create
+docker-compose run web rails db:migrate
+docker-compose run web rubocop -A
+docker-compose up
